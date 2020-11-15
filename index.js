7e232986d0cd5256ff6d2aff8803f0ee0da981f9
yongsheng.li <liyongsheng@me.com>
2020-11-15 15:42:05 +0800

    publish v0.1.1

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+const utils = require('loader-utils')
+
+module.exports = async function(source, map) {
+    
+    this.cacheable()
+    
+    source = `<script lang='ts'>${source}</script>`;
+
+    try {
+        let isJade = utils.getOptions(this).template == 'jade',
+            templatePath = this.resourcePath.replace('.tsx', isJade ? '.jade' : '.html')
+            template = String(fs.readFileSync(templatePath))
+        // jade render if template is jade
+        isJade && (template = require('jade').render(template))
+    
+        source = `<template>${template}</template>` + source;
+    }
+    catch(err) {
+        console.log(err)
+    }
+    
+    return source
+}
